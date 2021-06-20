628df40353c1b1595769af60b39574a767199721
Reinan Gabriel <reinangabriel1520@gmail.com>
2021-06-20 16:03:13 -0300

    refactor: adds new employee entity

---
+++ src/main/java/com/reinanhs/gestorprojetos/entitys/funcionario/Funcionario.java
@@ -0,0 +1,29 @@
+package com.reinanhs.gestorprojetos.entitys.funcionario;
+
+import com.reinanhs.gestorprojetos.entitys.cargo.Cargo;
+import com.reinanhs.gestorprojetos.entitys.helpers.Pessoa;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+public class Funcionario extends Pessoa {
+    @Column(nullable = false, name = "data_admissacao")
+    @NotBlank(message = "O campo data de admissão é obrigatório")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    protected LocalDate dataAdmissao;
+
+    @Column(name = "data_demissao")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    protected LocalDate dataDemissao;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "cargo_id_fk", nullable = false)
+    protected Cargo cargo;
+}
