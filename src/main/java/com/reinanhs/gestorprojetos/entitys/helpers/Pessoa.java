4932e552397db53728750dfe1dad9d7a018e0f48
Reinan Gabriel <reinangabriel1520@gmail.com>
2021-06-20 13:42:01 -0300

    feat: adds new entity

---
+++ src/main/java/com/reinanhs/gestorprojetos/entitys/helpers/Pessoa.java
@@ -0,0 +1,42 @@
+package com.reinanhs.gestorprojetos.entitys.helpers;
+
+import com.reinanhs.gestorprojetos.entitys.Entidade;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@MappedSuperclass
+public class Pessoa extends Entidade {
+    @Column(nullable = false, length = 80)
+    @Size(min = 2, max = 80, message = "O tamanho inserido para o nome é inválido")
+    @NotBlank(message = "O campo nome é obrigatório")
+    protected String nome;
+
+    @Column(nullable = false, length = 14, unique = true)
+    @Size(min = 11, max = 14, message = "O tamanho inserido para o cpf é inválido")
+    @NotBlank(message = "O campo cpf é obrigatório")
+    protected String cpf;
+
+    @Column(nullable = false, length = 15)
+    @Size(min = 11, max = 15, message = "O tamanho inserido para o telefone é inválido")
+    @NotBlank(message = "O campo telefone é obrigatório")
+    protected String telefone;
+
+    @Column(nullable = false, length = 80, unique = true)
+    @Size(min = 10, max = 80, message = "O tamanho inserido para o email é invalido")
+    @NotBlank(message = "O campo email é obrigatório")
+    protected String email;
+
+    @Column(nullable = false, name = "data_nascimento")
+    @NotBlank(message = "O campo data de nascimento é obrigatório")
+    protected LocalDate dataNascimento;
+}
