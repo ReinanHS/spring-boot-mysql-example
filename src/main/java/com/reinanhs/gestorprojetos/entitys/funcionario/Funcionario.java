4bc2e2f522b5c56b669badcd69b79c66560bc6c9
Reinan Gabriel <reinangabriel1520@gmail.com>
2021-06-20 18:48:57 -0300

    fix: change connection type

--- src/main/java/com/reinanhs/gestorprojetos/entitys/funcionario/Funcionario.java
+++ src/main/java/com/reinanhs/gestorprojetos/entitys/funcionario/Funcionario.java
@@ -8,7 +8,7 @@ import lombok.EqualsAndHashCode;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -17,7 +17,7 @@ import java.util.List;
 @Data
 public class Funcionario extends Pessoa {
     @Column(nullable = false, name = "data_admissacao")
-    @NotBlank(message = "O campo data de admissão é obrigatório")
+    @NotNull(message = "O campo data de admissão é obrigatório")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     protected LocalDate dataAdmissao;
 
@@ -25,8 +25,9 @@ public class Funcionario extends Pessoa {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     protected LocalDate dataDemissao;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "cargo_id_fk", nullable = false)
+    @NotNull(message = "O campo cargo é obrigatório")
     protected Cargo cargo;
 
     @ManyToMany(mappedBy = "equipe", fetch = FetchType.LAZY)
