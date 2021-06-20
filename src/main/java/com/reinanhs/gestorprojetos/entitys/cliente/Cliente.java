1431a19711f06c1d55e1a40663ecec286c5b0385
Reinan Gabriel <reinangabriel1520@gmail.com>
2021-06-20 16:32:31 -0300

    refactor: add relationship

--- src/main/java/com/reinanhs/gestorprojetos/entitys/cliente/Cliente.java
+++ src/main/java/com/reinanhs/gestorprojetos/entitys/cliente/Cliente.java
@@ -1,14 +1,19 @@
 package com.reinanhs.gestorprojetos.entitys.cliente;
 
 import com.reinanhs.gestorprojetos.entitys.helpers.Pessoa;
+import com.reinanhs.gestorprojetos.entitys.projeto.Projeto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 public class Cliente extends Pessoa {
-
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
+    protected List<Projeto> projetos;
 }
